b8c6444b343fff449298ab545f0d2dcbc8233f0c
takatoshi.tenko <chanten811@gmail.com>
2022-10-05 00:27:28 +0900

    ページ内に絞ってsassが欠けることを確認した

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1 class="heading">Takatoshi Tenko</h1>
+
+    <Tutorial />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import '~/assets/css/style.scss'
 
 export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
+
+<style lang="scss">
+.heading {
+  text-align: center;
+  font-weight: bold;
+  font-size: 24px;
+  color: skyblue;
+}
+</style>
