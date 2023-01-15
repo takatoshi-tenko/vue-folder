19689cec001c54e3f0e80d3db38eed84de0d6791
takatoshi.tenko <chanten811@gmail.com>
2023-01-16 03:30:58 +0900

    fight-appの環境構築を行なった

---
+++ components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>
+    2023年は一旦年収1000万稼いどきます。
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translateY(-50%);
+  transform: translateX(-50%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Hello/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
