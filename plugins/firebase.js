50c2ef647724fab256f820794c93f0cf5e4f419b
takatoshi.tenko <chanten811@gmail.com>
2023-02-05 20:36:30 +0900

    firebase setup

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,24 +1,26 @@
-import Firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// if (!Firebase.apps.length) {
-//   Firebase.initializeApp({
-//     apiKey: "AIzaSyBu2YCzfbZKfZHH44mLxjyLw_TmhaYvvKc",
-//     authDomain: "fight-app-f2fcb.firebaseapp.com",
-//     projectId: "fight-app-f2fcb",
-//     storageBucket: "fight-app-f2fcb.appspot.com",
-//     messagingSenderId: "243159340568",
-//     appId: "1:243159340568:web:c8872d13e8e4b9ee2e556d",
-//     measurementId: "G-M127NPBND9"
-//   });
-// }
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBu2YCzfbZKfZHH44mLxjyLw_TmhaYvvKc",
+  authDomain: "fight-app-f2fcb.firebaseapp.com",
+  projectId: "fight-app-f2fcb",
+  storageBucket: "fight-app-f2fcb.appspot.com",
+  messagingSenderId: "243159340568",
+  appId: "1:243159340568:web:c8872d13e8e4b9ee2e556d",
+  measurementId: "G-M127NPBND9"
+};
 
-// export default (context, inject) => {
-//   const $fb = {
-//     app: Firebase.app,
-//     auth: Firebase.auth,
-//     firestore: Firebase.firestore,
-//   }
-//   inject('fb',$fb)
-// }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebase
