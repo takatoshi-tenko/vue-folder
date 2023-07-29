d5752af9df949efe743c507058c49c3421cefe55
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 23:26:31 +0900

    リスト6.11 カウンタ変数を利用したループ

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,25 +1,9 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const cocktailDataListInit: Cocktail[] = [
-  { id: 2345, name: 'ホワイトレディ', price: 1200 },
-  { id: 4412, name: 'ブルーハワイ', price: 1500 },
-  { id: 3364, name: 'ニューヨーク', price: 1100 },
-  { id: 7768, name: 'マティーニ', price: 1500 }
-]
-const cocktailDataList = ref(cocktailDataListInit)
-
-interface Cocktail {
-  id: number
-  name: string
-  price: number
-}
-</script>
-
 <template>
-  <div>
-    <li v-for="cocktailItem in cocktailDataList" :key="cocktailItem.id">
-      {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}
-    </li>
-  </div>
+	<ul>
+		<li
+			v-for="r in 5"
+			v-bind:key="r">
+			半径{{r}}の円の円周: {{2 * r * 3.14}}
+		</li>
+	</ul>
 </template>
