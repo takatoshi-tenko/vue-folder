dd32877491cac8cc437222407aaaca17614ade4a
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 21:50:07 +0900

    リスト5.3　片方向のバインディング

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const inputNameModel = ref('双方向')
+const inputNameBind = ref('しんちゃん')
+const inputNameOn = ref('ななし')
+const onInputName = (event: Event): void => {
+  const element = event.target as HTMLInputElement
+  inputNameOn.value = element.value
+}
 </script>
 
 <template>
   <div>
-    <input type="text" v-model="inputNameModel" />
-    <p>{{ inputNameModel }}</p>
+    <input type="text" :value="inputNameBind" />
+    <br />
+    <input type="text" @input="onInputName" />
+    <p>{{ inputNameOn }}</p>
   </div>
 </template>
