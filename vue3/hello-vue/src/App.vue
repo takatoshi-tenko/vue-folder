a996e3c89809f9e2eabef46abc1a3cd0d032709d
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 23:09:44 +0900

    リスト6.7 Mapのループ

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,24 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const cocktailLIstInit: { [key: number]: string } = {
-  2345: 'ホワイトレディ',
-  4412: 'ブルーハワイ',
-  6792: 'ニューヨーク'
-}
-const cocktailList = ref(cocktailLIstInit)
+const cocktailListInit = new Map<number, string>()
+cocktailListInit.set(2345, 'ホワイトレディ')
+cocktailListInit.set(4412, 'ブルーハワイ')
+cocktailListInit.set(6792, 'ニューヨーク')
+const cocktailList = ref(cocktailListInit)
 </script>
 
 <template>
   <div>
     <ul>
-      <li v-for="(cocktailName, id) in cocktailList" :key="'cocktailList' + id">
+      <li v-for="[id, cocktailName] in cocktailList" :key="id">
         IDが{{ id }}のカクテルは{{ cocktailName }}
-      </li>
-    </ul>
-    <ul>
-      <li v-for="(cocktailName, id, index) in cocktailList" :key="'cocktailListWithIdx' + id">
-        {{ index + 1 }}: IDが {{ id }}のカクテルは{{ cocktailName }}
       </li>
     </ul>
   </div>
