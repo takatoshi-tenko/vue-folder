84d603eff562fa572cc6babf1f49ad2b9b7c35ea
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 21:42:10 +0900

    リスト5.1 双方向バインディングの実現

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,27 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const msg = ref('まだです！')
-const onEnterKey = (): void => {
-  msg.value = 'エンターキーが押下しました。'
-}
-const onUpKey = (): void => {
-  msg.value = 'エンターキーが上に押下しました。'
-}
-const onRightButtonClick = (): void => {
-  msg.value = 'ボタンが右クリックされました'
-}
-const onShiftClick = (): void => {
-  msg.value = 'シフトを押しながらクリックしました。'
-}
+const inputNameModel = ref('双方向')
 </script>
 
 <template>
   <div>
-    <p>{{ msg }}</p>
-    <input type="text" @keydown.enter="onEnterKey" /><br />
-    <input type="text" @keydown.up="onUpKey" /><br />
-    <button @click.right="onRightButtonClick">右クリック</button><br />
-    <button @click.shift="onShiftClick">シフトを押しながらクリック</button>
+    <input type="text" v-model="inputNameModel" />
+    <p>{{ inputNameModel }}</p>
   </div>
 </template>
