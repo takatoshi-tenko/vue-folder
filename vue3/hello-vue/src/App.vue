fa5f5777619a4ae0f6a7de9dc2a23b8b4e096067
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 22:54:18 +0900

    リスト6.6 連想配列のループ

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const cocktailListInit: string[] = ['ホワイトレディ', 'ブルーハワイ', 'ニューヨーク']
-const cocktailList = ref(cocktailListInit)
+const cocktailLIstInit: { [key: number]: string } = {
+  2345: 'ホワイトレディ',
+  4412: 'ブルーハワイ',
+  6792: 'ニューヨーク'
+}
+const cocktailList = ref(cocktailLIstInit)
 </script>
 
 <template>
   <div>
     <ul>
-      <li v-for="cocktailName in cocktailList" v-bind:key="cocktailName">
-        {{ cocktailName }}
+      <li v-for="(cocktailName, id) in cocktailList" :key="'cocktailList' + id">
+        IDが{{ id }}のカクテルは{{ cocktailName }}
       </li>
     </ul>
     <ul>
-      <li v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">
-        {{ cocktailName }}(インデックス{{ index }})
+      <li v-for="(cocktailName, id, index) in cocktailList" :key="'cocktailListWithIdx' + id">
+        {{ index + 1 }}: IDが {{ id }}のカクテルは{{ cocktailName }}
       </li>
     </ul>
   </div>
