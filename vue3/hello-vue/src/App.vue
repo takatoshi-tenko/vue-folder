41fd94d45feb09221307e0434fe68aad8ec82d80
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 18:57:48 +0900

    リスト4.12

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const msg = ref('未送信')
-const onFormSubmit = (): void => {
-  msg.value = '送信されました'
+const msg = ref('まだです！')
+const onEnterKey = (): void => {
+  msg.value = 'エンターキーが押下しました。'
+}
+const onUpKey = (): void => {
+  msg.value = 'エンターキーが上に押下しました。'
+}
+const onRightButtonClick = (): void => {
+  msg.value = 'ボタンが右クリックされました'
+}
+const onShiftClick = (): void => {
+  msg.value = 'シフトを押しながらクリックしました。'
 }
 </script>
 
 <template>
   <div>
-    <form action="#" @submit.prevent="onFormSubmit">
-      <input type="text" required />
-      <button type="submit">送信</button>
-    </form>
     <p>{{ msg }}</p>
+    <input type="text" @keydown.enter="onEnterKey" /><br />
+    <input type="text" @keydown.up="onUpKey" /><br />
+    <button @click.right="onRightButtonClick">右クリック</button><br />
+    <button @click.shift="onShiftClick">シフトを押しながらクリック</button>
   </div>
 </template>
