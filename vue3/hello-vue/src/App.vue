62bad49753b4fbe0d6fb815e904f0440fb9cdd60
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 23:22:27 +0900

    リスト6.9 オブジェクトの配列のループ

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const whiteLadyInit: {
+const cocktailDataListInit: Cocktail[] = [
+  { id: 2345, name: 'ホワイトレディ', price: 1200 },
+  { id: 4412, name: 'ブルーハワイ', price: 1500 },
+  { id: 3364, name: 'ニューヨーク', price: 1100 },
+  { id: 7768, name: 'マティーニ', price: 1500 }
+]
+const cocktailDataList = ref(cocktailDataListInit)
+
+interface Cocktail {
   id: number
   name: string
   price: number
-  recipe: string
-} = {
-  id: 2345,
-  name: 'ホワイトレディ',
-  price: 1200,
-  recipe: 'ジン30ml＋個ワントロー15ml＋レモン果汁＋15ml'
 }
-const whiteLady = ref(whiteLadyInit)
 </script>
 
 <template>
   <div>
-    <template v-for="(value, key) in whiteLady" :key="key">
-      <dt>{{ key }}</dt>
-      <dd>{{ value }}</dd>
-    </template>
+    <li v-for="cocktailItem in cocktailDataList" :key="cocktailItem.id">
+      {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}
+    </li>
   </div>
 </template>
