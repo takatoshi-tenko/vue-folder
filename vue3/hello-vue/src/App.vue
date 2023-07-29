07f63e28c8802b484c22e26d3d7f72370bb5a5e2
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 18:03:37 +0900

    リスト4.7

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,72 +1,15 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-
-const msg = ref('こんにちは！世界')
-const isTextColorRed = ref(true)
-const isTextColorBlue = ref(false)
-const styles = ref({
-  isTextColorRed: false,
-  isTextColorBlue: true
-})
-
-const computedStyles = computed((): { textColorRed: boolean; bgColorBlue: boolean } => {
-  const randText = Math.round(Math.random())
-  let textColorFlg = true
-  if (randText == 0) {
-    textColorFlg = false
-  }
-  const randBg = Math.round(Math.random())
-  let bgColorFlg = true
-  if (randBg == 0) {
-    bgColorFlg = false
-  }
-  return {
-    textColorRed: textColorFlg,
-    bgColorBlue: bgColorFlg
-  }
-})
+const randValue = ref('まだです！')
+const onButtonClick = (): void => {
+  const rand = Math.round(Math.random() * 10)
+  randValue.value = String(rand)
+}
 </script>
 
 <template>
   <div>
-    <p v-bind:class="{ textColorRed: true, bgColorBlue: true }">
-      {{ msg }}
-    </p>
-    <p v-bind:class="{ textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue }">
-      {{ msg }}
-    </p>
-    <p v-bind:class="{ textColorPink: true }">
-      {{ msg }}
-    </p>
-    <p v-bind:class="{ 'text-color-pink': true }">
-      {{ msg }}
-    </p>
-    <p
-      class="textSize24"
-      v-bind:class="{ textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue }"
-    >
-      {{ msg }}
-    </p>
-    <p class="textSize24" v-bind:class="styles">
-      {{ msg }}
-    </p>
-    <p v-bind:class="computedStyles">
-      {{ msg }}
-    </p>
+    <button @click="onButtonClick">くりっく！</button>
+    <p>クリックの結果：{{ randValue }}</p>
   </div>
 </template>
-
-<style>
-.textColorRed {
-  color: red;
-}
-.text-color-pink {
-  color: pink;
-}
-.bgColorBlue {
-  background-color: blue;
-}
-.textSize24 {
-  font-size: 24px;
-}
-</style>
