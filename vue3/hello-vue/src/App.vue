46869124acde38e019ccddc9f0e1bcc635847388
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 17:14:22 +0900

    リスト4.4

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-const widthOrHeight = ref('Height')
-const widthOrHeightValue = ref(100)
+const imgAttributes = ref({
+  src: '/images/logo.svg',
+  alt: 'VUeのロゴ',
+  width: 75,
+  height: 75
+})
 </script>
 
 <template>
   <div>
-    <p><img src="./assets/logo.svg" alt="VueLogo" :[widthOrHeight]="widthOrHeightValue" /></p>
+    <p><img :="imgAttributes" /></p>
+    <p><img v-bind="imgAttributes" title="ろごです！" /></p>
+    <p><img v-bind="imgAttributes" alt="ロゴです！" /></p>
   </div>
 </template>
