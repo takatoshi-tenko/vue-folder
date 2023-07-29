bfb0a4aafb8c01888cffdb996d6b31e5fdee99ac
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 16:45:50 +0900

    リスト4.3

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import {ref} from "vue"
-const url = ref('https://vuejs.org/')
+import { ref } from "vue"
 
+const isSendButtonDisabled = ref(false)
 </script>
 
 <template>
   <div>
-    <p><a :href="url" target="_blank">Vue.jsのサイト</a></p>
-    <p><a :href="url + 'guide/introduction.html'" target="_blank">Vue.jsガイドのページ</a></p>
+    <p><button type="button" :disabled="isSendButtonDisabled">送信</button></p>
   </div>
 </template>
