fa0905782cc98875914ae74ddceab70b0f743078
takatoshi.tenko <chanten811@gmail.com>
2023-07-29 17:42:31 +0900

    リスト4.5

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,17 +1,55 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const imgAttributes = ref({
-  src: '/images/logo.svg',
-  alt: 'VUeのロゴ',
-  width: 75,
-  height: 75
+import { ref, computed } from 'vue'
+
+const msg = ref('こんにちは!世界')
+const msgTextRed = ref('red')
+const msgTextColor = ref('white')
+const msgBgColor = ref('black')
+const msgStyles = ref({
+  color: 'white',
+  backgroundColor: 'black'
+})
+const msgStyles2 = ref({
+  fontSize: '24pt'
+})
+const msgStyles3 = ref({
+  color: 'pink',
+  fontSize: '24pt'
+})
+const textSize = computed((): string => {
+  const size = Math.round(Math.random() * 25) + 10
+  return `${size}pt`
 })
 </script>
 
 <template>
   <div>
-    <p><img :="imgAttributes" /></p>
-    <p><img v-bind="imgAttributes" title="ろごです！" /></p>
-    <p><img v-bind="imgAttributes" alt="ロゴです！" /></p>
+    <p v-bind:style="{ color: msgTextRed }">
+      {{ msg }}
+    </p>
+    <p v-bind:style="{ color: 'pink' }">
+      {{ msg }}
+    </p>
+    <p v-bind:style="{ fontSize: textSize }">
+      {{ msg }}
+    </p>
+    <p v-bind:style="{ color: msgTextColor, backgroundColor: msgBgColor }">
+      {{ msg }}
+    </p>
+    <p v-bind:style="{ color: msgTextColor, 'background-color': msgBgColor }">
+      {{ msg }}
+    </p>
+    <p v-bind:style="msgStyles">
+      {{ msg }}
+    </p>
+    <p v-bind:style="[msgStyles, msgStyles2]">
+      {{ msg }}
+    </p>
+    <p v-bind:style="[msgStyles, msgStyles3]">
+      {{ msg }}
+    </p>
+    <p v-bind:style="[msgStyles3, msgStyles]">
+      {{ msg }}
+    </p>
   </div>
 </template>
