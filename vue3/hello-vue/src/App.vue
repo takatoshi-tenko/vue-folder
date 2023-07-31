03d0f0b3257eb7ca2458c14a94e23ed47999791b
takatoshi.tenko <chanten811@gmail.com>
2023-07-31 18:23:07 +0900

    リスト7.1 算出プロパティでの処理

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -2,34 +2,24 @@
 import { ref, computed } from 'vue'
 
 const cocktailDataListInit = new Map<number, Cocktail>()
-cocktailDataListInit.set(2345, { id: 2345, name: 'ホワイトレディ', price: 1200 })
-cocktailDataListInit.set(4412, { id: 4412, name: 'ブルーハワイ', price: 1500 })
-cocktailDataListInit.set(6792, { id: 6792, name: 'ニューヨーク', price: 1100 })
-cocktailDataListInit.set(8429, { id: 8429, name: 'マティーニ', price: 1500 })
-const cocktailDataList = ref(cocktailDataListInit)
-const cocktail1500 = computed(
-  //算出関数。戻り値の型はMap。
-  (): Map<number, Cocktail> => {
-    //絞り込んだ要素を格納する新しいMapを用意。
-    const newList = new Map<number, Cocktail>()
-    //cocktailDataList内のMapをループ処理。
-    cocktailDataList.value.forEach(
-      //ループの各処理内容を表す関数。
-      (value: Cocktail, key: number): void => {
-        //カクテルの価格が1500円ならnewListに登録。
-        if (value.price == 1500) {
-          newList.set(key, value)
-        }
-      }
-    )
-    //絞り込んだ新しいMapを戻り値とする。
-    return newList
+cocktailDataListInit.set(1, { id: 1, name: 'ホワイトレディ', price: 1200 })
+cocktailDataListInit.set(2, { id: 2, name: 'ブルーハワイ', price: 1400 })
+cocktailDataListInit.set(3, { id: 3, name: 'ニューヨーク', price: 1800 })
+cocktailDataListInit.set(4, { id: 4, name: 'マティーニ', price: 1100 })
+
+const cocktailNo = ref(1)
+const priceMsg = computed((): string => {
+  const cocktail = cocktailDataListInit.get(cocktailNo.value)
+  let msg = '該当カクテルはありません。'
+  if (cocktail != undefined) {
+    msg = `該当カクテルは${cocktail.name}で、価格は${cocktail.price}円です。`
   }
-)
-const changeWhiteLadyPriceInList = (): void => {
-  const whiteLady = cocktailDataList.value.get(2345) as Cocktail
-  whiteLady.price = 1500
-}
+  return msg
+})
+
+setInterval((): void => {
+  cocktailNo.value = Math.round(Math.random() * 3) + 1
+}, 1000)
 
 interface Cocktail {
   id: number
@@ -40,25 +30,7 @@ interface Cocktail {
 
 <template>
   <div>
-    <section>
-      全てのカクテルリスト
-      <ul>
-        <li v-for="[id, cocktailItem] in cocktailDataList" v-bind:key="id">
-          {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
-        </li>
-      </ul>
-    </section>
-    <section>
-      値段が1500円のカクテルリスト
-      <ul>
-        <li v-for="[id, cocktailItem] in cocktail1500" v-bind:key="'cocktail1500' + id">
-          {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
-        </li>
-      </ul>
-    </section>
-    <p>
-      CocktailDataList内のホワイトレディの価格を1500円に
-      <button v-on:click="changeWhiteLadyPriceInList">変更</button>
-    </p>
+    <p>現在のカクテル番号：{{ cocktailNo }}</p>
+    <p>{{ priceMsg }}</p>
   </div>
 </template>
