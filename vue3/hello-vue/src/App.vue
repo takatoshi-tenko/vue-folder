864f5f14218909573cc9485a36b3ab75fde505ee
takatoshi.tenko <chanten811@gmail.com>
2023-07-31 23:51:40 +0900

    リスト7.10 toRefsの記述用法

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,45 +1,29 @@
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
+import { defineComponent, reactive, computed, toRefs } from 'vue'
 
 export default defineComponent({
   name: 'App',
   setup() {
-    const cocktailDataListInit = new Map<number, Cocktail>()
-    cocktailDataListInit.set(1, { id: 1, name: 'ホワイトレディ', price: 1200 })
-    cocktailDataListInit.set(2, { id: 2, name: 'ブルーハワイ', price: 1500 })
-    cocktailDataListInit.set(3, { id: 3, name: 'ニューヨーク', price: 1100 })
-    cocktailDataListInit.set(4, { id: 4, name: 'マティーニ', price: 1500 })
+    const data = reactive({
+      PI: 3.14,
+      radius: Math.round(Math.random() * 10)
+    })
 
-    const cocktailNo = ref(1)
-    const priceMsg = computed((): string => {
-      const cocktail = cocktailDataListInit.get(cocktailNo.value)
-      let msg = '該当カクテルはありません。'
-      if (cocktail != undefined) {
-        msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`
-      }
-      return msg
+    const area = computed((): number => {
+      return data.radius * data.radius * data.PI
     })
 
     setInterval((): void => {
-      cocktailNo.value = Math.round(Math.random() * 3) + 1
+      data.radius = Math.round(Math.random() * 3) + 1
     }, 1000)
     return {
-      cocktailNo,
-      priceMsg
+      ...toRefs(data),
+      area
     }
   }
 })
-
-interface Cocktail {
-  id: number
-  name: string
-  price: number
-}
 </script>
 
 <template>
-  <div>
-    <p>現在のカクテル番号: {{ cocktailNo }}</p>
-    <p>{{ priceMsg }}</p>
-  </div>
+  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
 </template>
