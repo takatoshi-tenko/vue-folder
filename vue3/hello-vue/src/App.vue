0a53c5ffc36cc760fe083ebb2b09139600b59b7e
takatoshi.tenko <chanten811@gmail.com>
2023-08-01 00:14:14 +0900

    リスト8.3 コンポーネントの独立性

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -1,26 +1,21 @@
 <script setup lang="ts">
-import OneSection from './components/OneSection.vue'
+import WithModel from './components/WithModel.vue'
 </script>
 
 <template>
   <div>
-    <h1>コンポーネント基礎</h1>
+    <h1>コンポーネントの独立性</h1>
     <section>
-      <h2>コンポーネント1個</h2>
-      <OneSection />
-    </section>
-    <section>
-      <h2>コンポーネントが複数</h2>
-      <OneSection />
-      <OneSection />
-      <OneSection />
+      <h2>v-modelを含むコンポーネント</h2>
+      <WithModel />
+      <WithModel />
     </section>
   </div>
 </template>
 
 <style>
 section {
-	border: blue 1px solid;
-	margin: 10px;
+  border: blue 1px solid;
+  margin: 10px;
 }
 </style>
