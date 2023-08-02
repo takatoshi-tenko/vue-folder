d04f5fc5662046e6ab8110868f5f36c04e36e77c
takatoshi.tenko <chanten811@gmail.com>
2023-08-02 22:29:01 +0900

    リスト8.14 子から親への通信emit

--- vue3/hello-vue/src/components/OneSection.vue
+++ vue3/hello-vue/src/components/OneSection.vue
@@ -1,15 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  rand: number
+}
+
+interface Emits {
+  (event: 'createNewRand'): void
+}
+
+defineProps<Props>()
+const emit = defineEmits<Emits>()
+const onNewRandButtonClick = (): void => {
+  emit('createNewRand')
+}
+</script>
+
 <template>
   <div>
-    <section class="box">
-      <h4>一つのコンポーネント</h4>
-      <p>コンポーン年とについて〜〜〜</p>
+    <section>
+      <p>子コンポーネントで乱数を表示{{ rand }}</p>
+      <button @click="onNewRandButtonClick">新たな乱数を発生</button>
     </section>
   </div>
 </template>
-
-<style>
-  .box {
-    border: green 1px solid;
-    margin: 10px;
-  }
-</style>
