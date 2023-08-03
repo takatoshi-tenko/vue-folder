0393dbad087d1d55cc646041c97939b701fb0c9c
takatoshi.tenko <chanten811@gmail.com>
2023-08-04 08:32:05 +0900

    リスト10.7 ルーティング処理の実装

---
+++ vue3/vue-router/src/views/AppTop.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>TOP</h1>
+    <nav id="breadcrumbs">
+      <ul>
+        <li>TOP</li>
+      </ul>
+    </nav>
+    <section>
+      <p>
+        <RouterLink :to="{ name: 'MemberList' }"> 会員管理はこちら </RouterLink>
+      </p>
+    </section>
+  </div>
+</template>
---
+++ vue3/vue-router/src/views/member/MemberList.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>会員登録</h1>
+    <section>
+      <h2>会員リスト</h2>
+    </section>
+  </div>
+</template>
