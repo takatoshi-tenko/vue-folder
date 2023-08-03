e17e31ae470bee80bc04828beadacd01f62b9b61
takatoshi.tenko <chanten811@gmail.com>
2023-08-03 23:28:36 +0900

    リスト9.7 親でのレンダリング結果のslotを確認

--- vue3/hello-vue/src/App.vue
+++ vue3/hello-vue/src/App.vue
@@ -2,15 +2,21 @@
 import { ref } from 'vue'
 import OneSection from './components/OneSection.vue'
 
+const taroProblemsInit: string[] = ['電話が通じません。', '留守です。']
 const taro = ref('田中太郎')
+const taroProblems = ref(taroProblemsInit)
 const jiro = ref('鈴木二郎')
 </script>
 
 <template>
   <section>
     <h2>Slotの利用</h2>
-    <OneSection v-bind:name="taro">
-      <p>{{ taro }}さんは連絡がつきません。</p>
+    <OneSection :name="taro">
+      <ul>
+        <li v-for="problem in taroProblems" :key="problem">
+          {{ problem }}
+        </li>
+      </ul>
     </OneSection>
     <OneSection :name="jiro" />
   </section>
