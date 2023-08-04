b25fbd3c781ab368e3aba48b26c7c68ad9ae80da
takatoshi.tenko <chanten811@gmail.com>
2023-08-04 23:24:00 +0900

    ルーティング処理の続き

--- vue3/vue-router/src/router/index.ts
+++ vue3/vue-router/src/router/index.ts
@@ -15,6 +15,19 @@ const routeSettings: RouteRecordRaw[] = [
       return import("@/views/member/MemberList.vue");
     },
   },
+  {
+    path: "/member/detail/:id",
+    name: "MemberDetail",
+    component: () => {
+      return import("@/views/member/MemberDetail.vue");
+    },
+    props: (routes) => {
+      const idNum = Number(routes.params.id)
+      return {
+        id: idNum
+      }
+    }
+  },
 ];
 
 const router = createRouter({
