8e8700b2da3821425d072327cc45d49b0602ade2
takatoshi.tenko <chanten811@gmail.com>
2023-08-05 00:20:30 +0900

    storeの利用

--- vue3/pinia-basic/src/stores/counter.ts
+++ vue3/pinia-basic/src/stores/counter.ts
@@ -1,12 +1,35 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+// export const useCounterStore = defineStore('counter', () => {
+//   const count = ref(0)
+//   const doubleCount = computed(() => count.value * 2)
+//   function increment() {
+//     count.value++
+//   }
 
-  return { count, doubleCount, increment }
-})
+//   return { count, doubleCount, increment }
+// })
+
+interface State {
+  counter: number;
+}
+
+export const useCounterStore = defineStore({
+  id: "counter",
+  state: (): State => {
+    return {
+      counter: 0,
+    };
+  },
+  getters: {
+    doubleCount: (state): number => {
+      return state.counter * 2;
+    },
+  },
+  actions: {
+    incrementCount(): void {
+      this.counter++;
+    },
+  },
+});
