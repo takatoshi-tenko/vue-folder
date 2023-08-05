7bcb1d6a4f6ae9687fdbf9d653e715daf1dabfa1
takatoshi.tenko <chanten811@gmail.com>
2023-08-05 13:05:34 +0900

    pinia-fundamentalの移設

--- vue3/async-db/src/App.vue
+++ vue3/async-db/src/App.vue
@@ -1,85 +1,36 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <header>
+      <h1>Piniaサンプル</h1>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+main {
+  border: blue 1px solid;
+  padding: 10px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#breadcrumbs ul li {
+  display: inline;
+  list-style-type: none;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+#breadcrumbs {
+  margin-left: 0px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#breadcrumbs ul {
+  padding-left: 0px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#breadcrumbs ul .current {
+  color: red;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+#breadcrumbs ul li:before {
+  content: " > ";
 }
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#breadcrumbs ul li:first-child:before {
+  content: none;
 }
 </style>
